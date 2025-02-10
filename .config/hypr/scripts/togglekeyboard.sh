89422f6a6dc3391d8aafaec6c9ca126d6bd67d32
Vivianne Coelho Fernandes <vivivivianne.coelho@gmail.com>
2025-02-10 18:32:26 -0300

    updated script

--- .config/hypr/scripts/togglekeyboard.sh
+++ .config/hypr/scripts/togglekeyboard.sh
@@ -5,21 +5,21 @@ export STATUS_FILE="$XDG_RUNTIME_DIR/keyboard.status"
 enable_keyboard() {
 	printf "true" >"$STATUS_FILE"
 	notify-send -u normal "Enabling Keyboard"
-	hyprctl keyword "$LAPTOP_KB_ENABLED" "true" -r
+	hyprctl keyword '$LAPTOP_KB_ENABLED' "true" -r
 }
 
 disable_keyboard() {
 	printf "false" >"$STATUS_FILE"
 	notify-send -u normal "Disabling Keyboard"
-	hyprctl keyword "$LAPTOP_KB_ENABLED" "false" -r
+	hyprctl keyword '$LAPTOP_KB_ENABLED' "false" -r
 }
 
 if ! [ -f "$STATUS_FILE" ]; then
 	enable_keyboard
 else
-	if [ "$(cat "$STATUS_FILE")" = "true" ]; then
+	if [ $(cat "$STATUS_FILE") = "true" ]; then
 		disable_keyboard
-	elif [ "$(cat "$STATUS_FILE")" = "false" ]; then
+	elif [ $(cat "$STATUS_FILE") = "false" ]; then
 		enable_keyboard
 	fi
 fi
