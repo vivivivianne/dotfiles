e8600b031f33c667f77b28d37a7d634124901c1a
Vivianne Coelho Fernandes <vivivivianne.coelho@gmail.com>
2025-05-19 06:43:08 -0300

    updated update script

--- gitdotfiles/scripts/update.sh
+++ gitdotfiles/scripts/update.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 #fetch new stuff, discard local changes, update
-git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" fetch
-git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" reset --hard HEAD 
-git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" merge origin/$CURRENT_BRANCH
+git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" fetch && git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" reset --hard HEAD && git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" merge origin/$CURRENT_BRANCH
 
 # Update WPGTK Symlinks, add WPGTK wallpapers, and generate a colorscheme
 $XDG_CONFIG_HOME/wpg/fix-symlinks.sh
