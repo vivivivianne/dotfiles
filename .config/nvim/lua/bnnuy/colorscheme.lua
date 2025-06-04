558dd216966041d18ae2a96dfc6f3fcd56f53b8e
MayonnaiseTraveler <fournandes@protonmail.com>
2025-06-04 06:35:52 -0300

    updated default files

--- .config/nvim/lua/bnnuy/colorscheme.lua
+++ .config/nvim/lua/bnnuy/colorscheme.lua
@@ -1,7 +1,7 @@
 -- Alternatively, you can provide a table specifying your colors to the setup function.
 require('base16-colorscheme').setup({
-    base00 = '#030a1c',base01 = '#06194a',base02 = '#585C95',base03 = '#EE99B5',
-    base04 = '#464992', base05 = '#d4cedf', base06 = '#59B1B9', base07 = '#d4cedf',
-    base08 = '#fcf4ff', base09 = '#f175db', base0A = '#636ad1', base0B = '#ffade2',
-    base0C = '#4c51cb', base0D = '#ff73b4', base0E = '#61f4ff', base0F = '#fcf4ff',
+    base00 = '#0e1723',base01 = '#1b2e47',base02 = '#295694',base03 = '#CF9779',
+    base04 = '#2460C0', base05 = '#e6c3c1', base06 = '#499ECD', base07 = '#e6c3c1',
+    base08 = '#ffebe8', base09 = '#607bce', base0A = '#286dcc', base0B = '#ffbd88',
+    base0C = '#1f79ff', base0D = '#f47fc9', base0E = '#4cd2ff', base0F = '#ffebe8',
 })
