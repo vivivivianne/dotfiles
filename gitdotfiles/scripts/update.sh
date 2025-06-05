f0599c336493b3d885e5bf8f88e8a85c9141ff66
MayonnaiseTraveler <fournandes@protonmail.com>
2025-06-05 10:49:07 -0300

    updated stuff

--- gitdotfiles/scripts/update.sh
+++ gitdotfiles/scripts/update.sh
@@ -6,10 +6,10 @@ chmod +x $HOME/gitdotfiles/scripts/*
 chmod +x $XDG_CONFIG_HOME/wpg/fix-symlinks.sh
 rm -rf $XDG_CONFIG_HOME/wpg/schemes/*
 rm -rf $XDG_CONFIG_HOME/wpg/wallpapers/*
+rm -rf $XDG_CONFIG_HOME/Wallpapers/*.gif.wal_sample.png
 
 # Update WPGTK Symlinks, add WPGTK wallpapers, and generate a colorscheme
 $XDG_CONFIG_HOME/wpg/fix-symlinks.sh
 wpg -a $XDG_CONFIG_HOME/Wallpapers/*
 wpg -A $(wpg -l)
-rm -rf $XDG_CONFIG_HOME/wpg/wallpapers/*.gif.wal_sample.png
 
