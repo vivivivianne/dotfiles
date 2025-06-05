beaa5085407dcfacb583ae7f9da3cf1e023f3e87
MayonnaiseTraveler <fournandes@protonmail.com>
2025-06-05 10:32:52 -0300

    updated install script

--- gitdotfiles/scripts/update.sh
+++ gitdotfiles/scripts/update.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #fetch new stuff, discard local changes, update
-git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" fetch && git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" reset --hard HEAD && git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" merge origin/main
-
+git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" fetch && git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" reset --hard HEAD && git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" merge origin main
 chmod +x $HOME/gitdotfiles/scripts/*
 chmod +x $XDG_CONFIG_HOME/wpg/fix-symlinks.sh
 rm -rf $XDG_CONFIG_HOME/wpg/schemes/*
