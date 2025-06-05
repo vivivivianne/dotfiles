e28f19f7f4e2a3c1e280805f8fcd77451605386f
MayonnaiseTraveler <fournandes@protonmail.com>
2025-06-05 10:27:31 -0300

    updated scripts

--- gitdotfiles/scripts/install.sh
+++ gitdotfiles/scripts/install.sh
@@ -9,6 +9,10 @@ git --git-dir="$HOME/.cfg/" --work-tree="$HOME" config --local status.showUntrac
 # Downloading the needed packages (+ some extra stuff you might not use)
 paru -S --needed $(echo ~/gitdotfiles/PC-Packages.txt)
 
+chmod +x $HOME/gitdotfiles/scripts/*
+chmod +x $XDG_CONFIG_HOME/wpg/fix-symlinks.sh
+rm -rf $XDG_CONFIG_HOME/wpg/schemes/*
+
 # Update WPGTK Symlinks, add WPGTK wallpapers, and generate a colorscheme
 $XDG_CONFIG_HOME/wpg/fix-symlinks.sh
 wpg -a $XDG_CONFIG_HOME/Wallpapers/*
--- gitdotfiles/scripts/update.sh
+++ gitdotfiles/scripts/update.sh
@@ -3,7 +3,12 @@
 #fetch new stuff, discard local changes, update
 git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" fetch && git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" reset --hard HEAD && git --git-dir="/$HOME/.cfg/" --work-tree="$HOME" merge origin/$CURRENT_BRANCH
 
+chmod +x $HOME/gitdotfiles/scripts/*
+chmod +x $XDG_CONFIG_HOME/wpg/fix-symlinks.sh
+rm -rf $XDG_CONFIG_HOME/wpg/schemes/*
+
 # Update WPGTK Symlinks, add WPGTK wallpapers, and generate a colorscheme
 $XDG_CONFIG_HOME/wpg/fix-symlinks.sh
 wpg -a $XDG_CONFIG_HOME/Wallpapers/*
 wpg -A $(wpg -l)
+
