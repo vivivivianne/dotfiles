9e8cc8e5707c99e055dd895546add430750107a0
Vivianne Coelho Fernandes <vivivivianne.coelho@gmail.com>
2025-07-18 02:23:16 -0300

    updated default config

--- .config/nvim/lua/bnnuy/colorscheme.lua
+++ .config/nvim/lua/bnnuy/colorscheme.lua
@@ -1,7 +1,7 @@
 -- Alternatively, you can provide a table specifying your colors to the setup function.
 require('base16-colorscheme').setup({
-    base00 = '#0e1723',base01 = '#1b2e47',base02 = '#295694',base03 = '#CF9779',
-    base04 = '#2460C0', base05 = '#e6c3c1', base06 = '#499ECD', base07 = '#e6c3c1',
-    base08 = '#ffebe8', base09 = '#607bce', base0A = '#286dcc', base0B = '#ffbd88',
-    base0C = '#1f79ff', base0D = '#f47fc9', base0E = '#4cd2ff', base0F = '#ffebe8',
+    base00 = '#0c0b15',base01 = '#1e1b36',base02 = '#584767',base03 = '#492953',
+    base04 = '#675D87', base05 = '#d6a7cd', base06 = '#5B3463', base07 = '#d6a7cd',
+    base08 = '#ffdbff', base09 = '#9e519e', base0A = '#735191', base0B = '#632d73',
+    base0C = '#7e6abe', base0D = '#9679c7', base0E = '#7c398a', base0F = '#ffdbff',
 })
