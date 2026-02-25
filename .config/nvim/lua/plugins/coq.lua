ee314c5c3c86d30e9e4fc1c6d2992484a6ede619
MayonnaiseTraveler <fournandes@protonmail.com>
2026-02-25 19:19:51 -0300

    quickfixes

--- .config/nvim/lua/plugins/coq.lua
+++ .config/nvim/lua/plugins/coq.lua
@@ -6,11 +6,11 @@ return {
 		{ "ms-jpq/coq_nvim", branch = "coq" },
 
 		-- 9000+ Snippets
-		-- { "ms-jpq/coq.artifacts",  branch = "artifacts" },
+		{ "ms-jpq/coq.artifacts",  branch = "artifacts" },
 
 		-- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
 		-- Need to **configure separately**
-		{ 'ms-jpq/coq.thirdparty', branch = "3p" }
+		-- { 'ms-jpq/coq.thirdparty', branch = "3p" }
 		-- - shell repl
 		-- - nvim lua api
 		-- - scientific calculator
@@ -26,7 +26,6 @@ return {
 			display = {
 				preview = {
 					border = "rounded",
-					positions = { north = null, south = null, west = null, east = 1 },
 				}
 			}
 		}
