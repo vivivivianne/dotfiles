2028ad3a7d045489eb01214224ae3d4b644af052
MayonnaiseTraveler <fournandes@protonmail.com>
2026-03-21 16:29:35 -0300

    fixed nvim css format

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,26 +1,26 @@
 return {
-	{'williamboman/mason.nvim', opts = {}},
-	{'williamboman/mason-lspconfig.nvim',opts = {}},
-	{'WhoIsSethDaniel/mason-tool-installer.nvim', 
+	{ "williamboman/mason.nvim", opts = {} },
+	{ "williamboman/mason-lspconfig.nvim", opts = {} },
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		opts = {
-		ensure_installed = {
-			"clang-format",
-			"prettier",
-			"prettierd",
-			"cpptools",
-			"cmakelang",
-			"lua-language-server",
-			"bash-language-server",
-			"html-lsp",
-			"eslint-lsp",
-			"json-lsp",
-			"cspell",
-			"pyright",
-			"quick-lint-js",
-			"tailwindcss-language-server",
-			"stylua",
-			"shfmt" }
-		} 
+			ensure_installed = {
+				"clang-format",
+				"cssls",
+				"prettierd",
+				"cpptools",
+				"cmakelang",
+				"lua-language-server",
+				"bash-language-server",
+				"html-lsp",
+				"eslint-lsp",
+				"json-lsp",
+				"cspell",
+				"pyright",
+				"quick-lint-js",
+				"stylua",
+				"shfmt",
+			},
+		},
 	},
-
 }
