30c4cb40f45706714c79e1ed797950b0f660a1ab
Vivianne Coelho Fernandes <vivivivianne.coelho@gmail.com>
2026-04-08 13:38:46 -0300

    changed to qt-engine, switch to secrets password manager

--- .config/nvim/ftplugin/c.lua
+++ .config/nvim/ftplugin/c.lua
@@ -1,5 +1,5 @@
 ---@diagnostic disable: undefined-global
 
 vim.opt.tabstop = 8
-vim.opt.shiftwidth = 8 -- the number of spaces inserted for each indentation
-
+vim.opt.shiftwidth = 8
+-- the number of spaces inserted for each indentation
