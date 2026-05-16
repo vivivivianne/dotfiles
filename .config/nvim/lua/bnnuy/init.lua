2229a893c92051309a1f6a042a6537f71a17314a
Vivianne Coelho Fernandes <vivivivianne.coelho@gmail.com>
2026-05-16 04:23:52 -0300

    fixed init.lua

--- .config/nvim/lua/bnnuy/init.lua
+++ .config/nvim/lua/bnnuy/init.lua
@@ -44,22 +44,24 @@ vim.filetype.add({
 
 local coq = require("coq")
 
-vim.lsp.config("clangd", coq.lsp_ensure_capabilities({
-  cmd = {
-    "clangd",
-    "--background-index",
-    "--query-driver=**", -- Or provide exact path to xtensa-esp32-elf-gcc
-    "--compile-commands-dir=build" -- Point to the directory containing compile_commands.json
-  },
-  -- Additional settings if needed
-}))
-
+vim.lsp.config(
+	"clangd",
+	coq.lsp_ensure_capabilities({
+		cmd = {
+			"clangd",
+			"--background-index",
+			"--query-driver=**", -- Or provide exact path to xtensa-esp32-elf-gcc
+			"--compile-commands-dir=build", -- Point to the directory containing compile_commands.json
+		},
+		-- Additional settings if needed
+	})
+)
 
 local null_ls = require("null-ls")
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.black,
-    },
+	sources = {
+		null_ls.builtins.formatting.black,
+	},
 })
 
 local dap = require("dap")
@@ -82,7 +84,6 @@ dap.adapters.c = {
 		args = { "--port", "${port}" },
 	},
 }
-
 
 vim.cmd([[autocmd Signal SIGUSR1 source ~/.config/nvim/lua/bnnuy/colorscheme.lua]])
 vim.cmd([[autocmd Signal SIGUSR1 source ~/.config/nvim/lua/bnnuy/init.lua]])
